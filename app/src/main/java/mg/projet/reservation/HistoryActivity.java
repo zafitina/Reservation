5ad1fee6d1fb54aa5b9ab8a929849c57a4c738a5
Nicolas <nicolas.zafitina@teleric.net>
2019-04-30 15:41:56 +0200

    Creation des pages home, search et history

--- app/src/main/java/mg/projet/reservation/HistoryActivity.java
+++ app/src/main/java/mg/projet/reservation/HistoryActivity.java
@@ -1,35 +1,13 @@
 package mg.projet.reservation;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.annotation.NonNull;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private Button btn_history;
-    private Button btn_search;
-    private View.OnClickListener btn_listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent;
-            switch (v.getId()) {
-                case R.id.btn_history :
-                    Log.d("ici", "historique");
-                    go_history();
-                    break;
-                case R.id.btn_search :
-                    Log.d("là", "recherche");
-                    go_search();
-                    break;
-            }
-        }
-    };
+public class HistoryActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -52,17 +30,12 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_history);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        btn_history = findViewById(R.id.btn_history);
-        btn_search = findViewById(R.id.btn_search);
-        btn_search.setOnClickListener(btn_listener);
-        btn_history.setOnClickListener(btn_listener);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
     /**
      * Go to seach activity
      */
